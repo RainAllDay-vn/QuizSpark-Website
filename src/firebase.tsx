fb9c06ba0ea0c9a31d53e2a597303f6eee19b1ef
Vũ Long <lnvlong.dev@gmail.com>
2025-10-24 22:36:38 +0700

    ♻️: Update FireBase authentication information

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-}
+  apiKey: "AIzaSyDXWEDdW5PpXhrl9oJkNMgbjQEje5KEp3k",
+  authDomain: "quizspark-f0fa7.firebaseapp.com",
+  projectId: "quizspark-f0fa7",
+  storageBucket: "quizspark-f0fa7.firebasestorage.app",
+  messagingSenderId: "993125948903",
+  appId: "1:993125948903:web:fb5cb269b86235fe3bf142",
+  measurementId: "G-L6KL77J1FJ"
+};
 
 export const app = initializeApp(firebaseConfig)
 export const analytics = getAnalytics(app)
